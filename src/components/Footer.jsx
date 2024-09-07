91f7a64ad65d5114f386eabaaae12d23af65d978
Lucas Feliciano <lucas.for.study.42@gmail.com>
2024-09-07 18:55:46 -0300

    styled footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,20 @@
-import './Footer.css';
+import styled from 'styled-components';
+
+const FooterWrapper = styled.footer`
+    text-align: center;
+    padding: 20px;
+    background-color: #f1f1f1;
+    color: #333;
+    font-size: 14px;
+`;
+
 const Footer = () => {
     return (
-        <h5>&#64;2024 - todos os direitos reservados</h5>
-    )
+        <FooterWrapper>
+            <h5>&#64;2024 - todos os direitos reservados</h5>
+        </FooterWrapper>
+    );
 }
-export default Footer
+
+export default Footer;
+
