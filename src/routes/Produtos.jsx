8fd9e66df4693186efb65c7fbce5dd3b2a3c9823
Rafael T <rt.lucena@outlook.com>
2024-09-07 20:07:23 -0300

    Adicionando os Produtos

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,23 +1,26 @@
 import { ProdutosWrapper, Card, CardTitle, CardDescription, CardImage } from '../css/ProdutosWrapper';
+import Luvas from '../assets/Luvas.jpg'
+import Placar from '../assets/Placar.jpg'
+import Tenis from '../assets/Tenis.jpg'
 
 const Produtos = () => {
     return (
         <ProdutosWrapper>
             <h1>Produtos</h1>
             <Card>
-                <CardImage src="https://via.placeholder.com/300" alt="Produto 1" />
-                <CardTitle>Produto 1</CardTitle>
-                <CardDescription>Descrição do Produto 1</CardDescription>
+                <CardImage src={Luvas} alt="Produto 1" />
+                <CardTitle>LUVA DE GOLEIRO POKER CHAMPION 8 EXTENDED</CardTitle>
+                <CardDescription>Descubra o próximo nível de desempenho e estilo com a Luva de Goleiro Poker Champion 8 Extended. Seu design ergonômico proporciona uma sensação de movimento e fluidez, permitindo que você se destaque em campo com confiança.</CardDescription>
             </Card>
             <Card>
-                <CardImage src="https://via.placeholder.com/300" alt="Produto 2" />
-                <CardTitle>Produto 2</CardTitle>
-                <CardDescription>Descrição do Produto 2</CardDescription>
+                <CardImage src={Placar} alt="Produto 2" />
+                <CardTitle>PLACAR DE MESA ATHI 31PTS E 7SETS - PRETO UN</CardTitle>
+                <CardDescription>O Placar de Mesa Multi Esportivo da marca Athi, é um item essencial para acompanhar e registrar os pontos em diversos jogos esportivos. Fabricado em PVC rígido na cor preta, esse placar é resistente e durável, garantindo sua longa vida útil. </CardDescription>
             </Card>
             <Card>
-                <CardImage src="https://via.placeholder.com/300" alt="Produto 3" />
-                <CardTitle>Produto 3</CardTitle>
-                <CardDescription>Descrição do Produto 3</CardDescription>
+                <CardImage src={Tenis} alt="Produto 3" />
+                <CardTitle>TÊNIS ASICS GEL-PULSE 15 FEMININO</CardTitle>
+                <CardDescription>O Tênis Asics Gel-Pulse 15 Feminino, é seu parceiro ideal para corridas leves e caminhadas, projetado para oferecer conforto e amortecimento. O cabedal em Jacquard mesh é um destaque deste modelo, proporcionando leveza e respirabilidade que reduzem a necessidade de construções adicionais, garantindo um ajuste confortável e arejado. A tecnologia GEL na parte traseira do calçado garante uma absorção de impacto excepcional, enquanto a entressola Flytefoam proporciona uma sensação de leveza e conforto sob os pés, ajudando a impulsionar sua performance para novos patamares.</CardDescription>
             </Card>
         </ProdutosWrapper>
     );
