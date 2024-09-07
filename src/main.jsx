dfa10ad0bb130b33c6b58e54aca30a1e328c57f5
Rafael T <rt.lucena@outlook.com>
2024-09-07 18:45:14 -0300

    Corrjindo Bug

--- src/main.jsx
+++ src/main.jsx
@@ -26,7 +26,6 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
     <RouterProvider router={router} />
   </StrictMode>,
 )
