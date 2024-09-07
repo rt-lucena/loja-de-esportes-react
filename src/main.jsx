24977496a386f79b1bc799dd7510a64e46488bdf
Rafael T <rt.lucena@outlook.com>
2024-09-07 18:22:11 -0300

    Criando as rotas dos componentes e instalando o react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,31 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Error from './routes/Error.jsx'
+import Home from './routes/Home.jsx'
+import Contato from './routes/Contato.jsx'
+import Produtos from './routes/Produtos.jsx'
+import Sobre from './routes/Sobre.jsx'
+
+const router = createBrowserRouter([
+  {
+    //Elemento Pai
+      path:'/',element:<App/>,
+      errorElement:<Error/>,
+
+    //Elemento Filho
+      children:[
+        {path:'/',element:<Home/>},
+        {path:'/contato',element:<Contato/>},
+        {path:'/produtos',element:<Produtos/>},
+        {path:'/sobre',element:<Sobre/>},
+      ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
