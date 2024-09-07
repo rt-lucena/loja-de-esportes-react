6a48b07dd10f974331772322c23532190b8d040d
Rafael T <rt.lucena@outlook.com>
2024-09-07 19:54:24 -0300

    Atualizando o Sobre

--- src/routes/Sobre.jsx
+++ src/routes/Sobre.jsx
@@ -1,9 +1,12 @@
 import { SobreWrapper, Titulo } from '../css/SobreWrapper';
+import Atleta from '../assets/Atleta.jpg'
 
 const Sobre = () => {
     return (
         <SobreWrapper>
             <Titulo>Sobre</Titulo>
+            <img src={Atleta} width={1080} height={720} alt="Atleta" />
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus nisi quia ut deleniti. Rem alias impedit similique corporis facilis culpa doloremque inventore, tenetur repellendus id eum, incidunt optio illo laudantium?</p>
         </SobreWrapper>
     );
 }
