22567fdad7dc71c7c6e2e6832cce6b5a729b9ac6
Rafael T <rt.lucena@outlook.com>
2024-09-07 21:55:31 -0300

    Ajuste 5

--- src/css/SobreWrapper.jsx
+++ src/css/SobreWrapper.jsx
@@ -3,6 +3,23 @@ import styled from 'styled-components';
 export const SobreWrapper = styled.div`
     padding: 20px;
     text-align: center;
+
+    border-radius: 4px;
+    background-color: #e0e0e0;
+    color: #333;
+    padding: 40px 20px;
+    text-align: center;
+    margin: 20px 0px 20px;
+
+    h1 {
+        font-size: 36px;
+        margin-bottom: 10px;
+    }
+
+    p {
+        font-size: 18px;
+        margin-bottom: 0;
+    }
 `;
 
 export const Titulo = styled.h1`
--- src/routes/Sobre.jsx
+++ src/routes/Sobre.jsx
@@ -6,7 +6,11 @@ const Sobre = () => {
         <SobreWrapper>
             <Titulo>Sobre</Titulo>
             <img src={Atleta} width={1080} height={720} alt="Atleta" />
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus nisi quia ut deleniti. Rem alias impedit similique corporis facilis culpa doloremque inventore, tenetur repellendus id eum, incidunt optio illo laudantium?</p>
+            <p>
+                Bem-vindo à nossa loja de esportes, onde a qualidade e a satisfação do cliente são nossas maiores prioridades! Oferecemos uma ampla variedade de produtos, desde bicicletas ergométricas até conjuntos de pesos e esteiras, todos selecionados para proporcionar o melhor desempenho e durabilidade.
+                Nosso compromisso é garantir que cada etapa da sua experiência de compra seja excepcional, desde a escolha dos produtos até a entrega rápida e eficiente. Nossa equipe de atendimento ao cliente está sempre pronta para ajudar, oferecendo suporte prestativo e comunicação fácil.
+                Se você está buscando equipamentos de alta performance para melhorar seu treino diário ou acessórios esportivos de qualidade, nossa loja é o lugar certo. Venha nos visitar e descubra como podemos ajudar você a alcançar seus objetivos esportivos com produtos de excelente custo-benefício.
+            </p>
         </SobreWrapper>
     );
 }
